5fcf11a4a2de92efa2ce9d13759f16489d39dc0e
Namrata Devkota <devkotanamrata03@gmail.com>
2024-03-19 11:06:37 +0545

    adding auditing feature

---
+++ Controllers/AuditController.cs
@@ -0,0 +1,54 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EmployeeManagement.Controllers
+{
+    public class AuditController : Controller
+    {
+        private readonly string connectionString;
+        public AuditController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("MVCConnectionString");
+        }
+
+        public IActionResult Index()
+        {
+            List<Audit> auditRecords = GetAuditrecords();
+            return View(auditRecords);
+        }
+        private List<Audit> GetAuditrecords()
+        {
+            List<Audit> auditRecords = new List<Audit>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM EmployeeAudit";
+                SqlCommand command = new SqlCommand(query, connection);
+               
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        auditRecords.Add(new Audit
+                        {
+                            AuditID = (int)reader["AuditID"],
+                            EmployeeID = (int)reader["EmployeeID"],
+                            OldName = reader["OldName"].ToString(),
+                            NewName = reader["NewName"].ToString(),
+                            ChangeType = reader["ChangeType"].ToString(),
+                            ChangeDate = (DateTime)reader["ChangeDate"],
+                            ChangedBy = reader["ChangedBy"].ToString()
+                        });
+                    }
+                }
+            }
+
+            return auditRecords;
+        }
+
+    }
+}
---
+++ Models/Audit.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Audit
+    {
+        public int AuditID { get; set; }
+        public int EmployeeID { get; set; }
+        public string OldName { get; set; }
+        public string NewName { get; set; }
+        public string ChangeType { get; set; }
+        public DateTime ChangeDate { get; set; }
+        public string ChangedBy { get; set; }
+    }
+}
