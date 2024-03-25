aaa94dc3f700629f544e43c9d745426387752ca5
Namrata Devkota <devkotanamrata03@gmail.com>
2024-03-25 20:30:09 +0545

    New changes

---
+++ Models/Email.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Email
+    {
+        public string Emailid { get; set; }
+        public string Emailname { get; set; }
+        public string EmailServer { get; set; }
+    }
+}
